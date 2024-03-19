6288bd23df794604e5ce1912c242dff8ae927c48
Ruthdeedee <113478045+Ruthdeedee@users.noreply.github.com>
2024-03-19 18:51:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Footer from "./components/Footer"
+import HowItWorks from "./components/HowItWorks"
+
+
+const App = () => {
+  
+     return (
+    
+     <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Footer/>
+      <HowItWorks/>
+     
+     </main>
+    
+  )
+}
+
+export default App
